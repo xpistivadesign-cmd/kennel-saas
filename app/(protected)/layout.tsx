42e7d00b267ef65193398ac1197fffbb5732f616
xpistivadesign-cmd <xpistivadesign@gmail.com>
2026-06-02 23:08:55 +0200

    Create layout.tsx

---
+++ app/(protected)/layout.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { supabase } from "../../lib/supabase";
+import { useRouter } from "next/navigation";
+
+export default function ProtectedLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    check();
+  }, []);
+
+  async function check() {
+    const { data } = await supabase.auth.getUser();
+
+    if (!data.user) {
+      router.push("/login");
+    } else {
+      setLoading(false);
+    }
+  }
+
+  if (loading) {
+    return (
+      <div style={{ padding: 20, textAlign: "center" }}>
+        <h3>Belépés ellenőrzése...</h3>
+      </div>
+    );
+  }
+
+  return <>{children}</>;
+}
