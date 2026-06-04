4dbb2640085fe159c8985a1db42cb59197086605
xpistivadesign-cmd <xpistivadesign@gmail.com>
2026-06-04 22:07:54 +0200

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,7 +1,7 @@
 import { cookies } from "next/headers";
 import { createServerClient } from "@supabase/ssr";
 
-export function createClient() {
+export function createServerSupabase() {
   const cookieStore = cookies();
 
   return createServerClient(
