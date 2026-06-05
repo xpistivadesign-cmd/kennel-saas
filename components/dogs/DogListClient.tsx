3e2870da084d6409ac5381f97e4a159968325798
xpistivadesign-cmd <xpistivadesign@gmail.com>
2026-06-05 12:46:18 +0200

    Update DogListClient.tsx

--- components/dogs/DogListClient.tsx
+++ components/dogs/DogListClient.tsx
@@ -1,1 +1,23 @@
+"use client";
 
+import Link from "next/link";
+
+export default function DogListClient({ dogs }: any) {
+  return (
+    <div className="p-4 space-y-2">
+      <h1 className="text-lg font-bold">Kutyák</h1>
+
+      <div className="grid gap-2">
+        {dogs.map((dog: any) => (
+          <Link
+            key={dog.id}
+            href={`/dogs/${dog.id}`}
+            className="p-3 border rounded hover:bg-gray-50"
+          >
+            <div className="font-medium">{dog.name}</div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
