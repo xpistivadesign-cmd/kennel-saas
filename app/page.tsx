6ae1ed918d2e39a74ecaecc312587844958c7a9a
xpistivadesign-cmd <xpistivadesign@gmail.com>
2026-06-08 22:44:16 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default async function HomePage() {
   } = await supabase.auth.getUser();
 
   if (user) {
-    redirect("/protected/dashboard");
+    redirect("/dashboard");
   }
 
   redirect("/login");
