d5ff8751102874f7cf0b9d0dc0b15a33a3c4868b
xpistivadesign-cmd <xpistivadesign@gmail.com>
2026-06-08 11:51:48 +0200

    Update ProgesteroneChart.tsx

--- components/charts/ProgesteroneChart.tsx
+++ components/charts/ProgesteroneChart.tsx
@@ -1,1 +1,29 @@
+"use client";
 
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+
+export default function ProgesteroneChart({
+  data,
+}: {
+  data: { test_date: string; value: number }[];
+}) {
+  return (
+    <div className="h-64 w-full">
+      <ResponsiveContainer>
+        <LineChart data={data}>
+          <XAxis dataKey="test_date" />
+          <YAxis />
+          <Tooltip />
+          <Line type="monotone" dataKey="value" stroke="#8884d8" />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+}
