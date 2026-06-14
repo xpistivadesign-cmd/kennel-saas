4e7223ec2659d8159a70dd4963123f419c33cb19
xpistivadesign-cmd <xpistivadesign@gmail.com>
2026-06-14 13:08:52 +0200

    Update supabase-server.ts

--- lib/db/supabase-server.ts
+++ lib/db/supabase-server.ts
@@ -1,26 +1,19 @@
 import { cookies } from "next/headers";
 import { createServerClient } from "@supabase/ssr";
 
-export function createSupabaseServer() {
-  const cookieStorePromise = cookies();
+export const createClient = () => {
+  const cookieStore = cookies();
 
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        async getAll() {
-          const cookieStore = await cookieStorePromise;
-          return cookieStore.getAll();
-        },
-        async setAll(cookiesToSet) {
-          const cookieStore = await cookieStorePromise;
-
-          cookiesToSet.forEach(({ name, value, options }) => {
-            cookieStore.set(name, value, options);
-          });
-        },
+        getAll: () => cookieStore.getAll(),
+        setAll: (c) => c.forEach(({ name, value, options }) =>
+          cookieStore.set(name, value, options)
+        ),
       },
     }
   );
-}
+};
